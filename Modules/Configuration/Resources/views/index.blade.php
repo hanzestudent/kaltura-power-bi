ddeb3bebe38df1e229e967f5ebb4a37e718073c8
hanzestudent <38101449+hanzestudent@users.noreply.github.com>
2018-04-25 10:02:30 +0200

    Configuration module added. This way i can add kaltura settings to the database

--- Modules/Configuration/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('configuration::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('configuration.name') !!}
-    </p>
-@stop
