17686627c2f312312cd1fe945e46a6b7654bd7c6
hanzestudent <38101449+hanzestudent@users.noreply.github.com>
2018-05-24 09:24:24 +0200

    added device in the database. Also added new presentations2GoMaps, because i received more from AV/Media.

--- Modules/Presentations2Go/Database/Migrations/2018_05_07_153320_create_recordings_table.php
+++ Modules/Presentations2Go/Database/Migrations/2018_05_07_153320_create_recordings_table.php
@@ -14,12 +14,13 @@ class CreateRecordingsTable extends Migration
     public function up()
     {
         Schema::create('recordings', function (Blueprint $table) {
-            $table->string('title',255)->nullable(false)->comment("Title of recording");
+            $table->string('title',255)->nullable(true)->comment("Title of recording");
             $table->text('description')->nullable(true)->comment("Description of recording");
             $table->string('tags')->nullable(true)->comment("Keyword of recording");
-            $table->string('creator_id')->nullable(false)->comment("id of user can be 4 digits");
-            $table->integer('duration')->nullable(false)->comment("Duration of recording");
-            $table->timestamp('recorded_at')->nullable(false)->comment("Recording has been recorded at");
+            $table->string('device')->nullable(true)->comment("name of device");
+            $table->string('creator_id')->nullable(true)->comment("id of user can be 4 digits");
+            $table->integer('duration')->nullable(true)->comment("Duration of recording");
+            $table->timestamp('recorded_at')->nullable(true)->comment("Recording has been recorded at");
             $table->index(['title','creator_id']);
         });
     }
